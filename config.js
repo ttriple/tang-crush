ead0a09c85096232a8624fbcae59672565ca7632
ttriple <86741461+ttriple@users.noreply.github.com>
2021-07-01 02:09:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Em bé à!',
+    introDesc: `Can I call you baby
+    Can you be my friend`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    title: 'Bé có yêu em như em yêy bé 🥺',
+    desc: 'Còn em thì đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Bé yêu em nhiều',
+    btnNo: 'Không, không yêu em',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bé yêu em  vậy 🥺',
+    btnReply: 'Gửi tình yêu',
+    reply: 'Em dễ thương lắm đó',
+    mess: 'Em biết ngay mà =))). Yêu bé ❤️',
+    messDesc: 'Bé thi xong mình đi chơi nhé 🌝',
+    btnAccept: 'Dạ vâng cục kẹo ❤️',
+    messLink: 'https://www.facebook.com/profile.php?id=100039057971305' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
